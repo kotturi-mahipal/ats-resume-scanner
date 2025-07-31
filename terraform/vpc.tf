723d52fcab765619a2540deca5a41bbb318d6a5b
kotturi-mahipal <mahipalkotturi6@gmail.com>
2025-07-31 13:40:49 +0530

    Remove ci and cd workflow

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,9 +8,9 @@ resource "aws_vpc" "main" {
 
 # Public Subnet
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  count = 2
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   availability_zone = ["us-east-1a", "us-east-1b"][count.index]
   map_public_ip_on_launch = true
 
@@ -34,7 +34,6 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
-
   tags = {
     Name = "ats-public-rt"
   }
@@ -43,7 +42,7 @@ resource "aws_route_table" "public" {
 
 # Assoc. route table with subnets
 resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
-  subnet_id = aws_subnet.public[count.index].id
-  count = 2
 }
